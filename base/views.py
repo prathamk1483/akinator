3f6e7c505b35e114a512ef81a36d4e51b21d47d7
prathamk1483 <133343956+prathamk1483@users.noreply.github.com>
2024-01-29 19:21:42 +0530

    Update views.py

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 import random
 from . models import *
+import os
 from django.conf import settings
 
 # Create your views here.
